4d4346a9989f98f1a2b8655a30a34d842acb1ac3
oxxik <113535465+oxxik@users.noreply.github.com>
2026-04-25 21:24:38 +0200

    Create kick.js

---
+++ kick-netlify/netlify/functions/kick.js
@@ -0,0 +1,12 @@
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ok: true,
+      message: "kick function działa"
+    })
+  };
+};
